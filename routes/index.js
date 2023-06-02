703d411264589f6e7608a173ba09b327be477700
jmespitiag <104176312+jmespitiag@users.noreply.github.com>
2023-06-02 13:39:17 -0500

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Proyecto Bases de Datos' });
 });
 
 module.exports = router;
