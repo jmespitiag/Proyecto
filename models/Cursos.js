93892f4e7bf2af112952523a91f88135719fa84e
jmespitiag <jmespitiag@eafit.edu.co>
2023-06-08 01:11:46 -0500

    Puede sewr pa?

---
+++ models/Cursos.js
@@ -0,0 +1,90 @@
+const conexion = require("../conexion");
+
+module.exports = {
+  listar() {
+    return new Promise((resolve, reject) => {
+      conexion.query('SELECT Nombre, categoria FROM Curso', (err, results) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(results);
+        }
+      });
+    });
+  },
+
+  listarProfesor(id_curso) {
+    return new Promise((resolve, reject) => {
+      conexion.query('SELECT Profesor.nombre, Profesor.email FROM Profesor INNER JOIN Dicta ON Profesor.doc_id = Dicta.doc_id INNER JOIN Curso ON Dicta.id_curso = Curso.id_curso WHERE Curso.id_curso = ?',
+        [id_curso],
+        (err, results) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(results);
+          }
+        }
+      );
+    });
+  },
+
+  listarAlumnos(id_curso) {
+    return new Promise((resolve, reject) => {
+      conexion.query('SELECT Usuario.nombre, Usuario.email FROM Usuario INNER JOIN Paga ON Usuario.doc_identidad = Paga.doc_identidad INNER JOIN Curso ON Paga.id_curso = Curso.id_curso WHERE Curso.id_curso = ?',
+        [id_curso],
+        (err, results) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(results);
+          }
+        }
+      );
+    });
+  },
+
+  listarMaterial(id_curso) {
+    return new Promise((resolve, reject) => {
+      conexion.query('SELECT DISTINCT titulo, descripcion, archivo FROM Material INNER JOIN Visualizar ON Material.id_mat = Visualizar.id_mat INNER JOIN Gestiona ON Material.id_mat = Gestiona.id_mat INNER JOIN Usuario ON Visualizar.id_nodo = Usuario.id_nodo INNER JOIN Profesor ON Gestiona.doc_id = Profesor.doc_id INNER JOIN Paga ON Usuario.doc_identidad = Paga.doc_identidad INNER JOIN Dicta ON Profesor.doc_id = Dicta.doc_id INNER JOIN Curso ON Dicta.id_curso = Curso.id_curso WHERE Curso.id_curso = ?',
+        [id_curso],
+        (err, results) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(results);
+          }
+        }
+      );
+    });
+  },
+
+  listarTarea(id_curso) {
+    return new Promise((resolve, reject) => {
+      conexion.query('SELECT DISTINCT Tarea.nombre, Tarea.descripcion, Tarea.fecha_e, Tarea.archivo FROM Tarea INNER JOIN Encarga ON Tarea.id_tarea = Encarga.id_tarea INNER JOIN Profesor ON Encarga.doc_id = Profesor.doc_id INNER JOIN Dicta ON Profesor.doc_id = Dicta.doc_id INNER JOIN Curso ON Dicta.id_curso = Curso.id_curso WHERE Curso.id_curso = ?',
+        [id_curso],
+        (err, results) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(results);
+          }
+        }
+      );
+    });
+  },
+
+  listarForo(id_curso) {
+    return new Promise((resolve, reject) => {
+      conexion.query('SELECT DISTINCT Foro.nombre, Foro.descripcion FROM Foro INNER JOIN Tiene ON Foro.id_foro = Tiene.id_foro INNER JOIN Curso ON Tiene.id_curso = Curso.id_curso WHERE Curso.id_curso = ?',
+        [id_curso],
+        (err, results) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(results);
+          }
+        }
+      );
+    });
+  }
+};
---
+++ routes/CursosRoutes.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+
+const CursoModel = require('../models/Cursos');
+
+router.get('/', (req, res, next) => {
+  CursoModel.listar()
+    .then((Curso) => {
+      res.render('Cursos/ver', {
+        Curso: Curso
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error obteniendo Grupo');
+    });
+});
+
+router.get('/:id_curso', async (req, res, next) => {
+  try {
+    const id_curso = req.params.id_curso;
+    const profesores = await CursoModel.listarProfesor(id_curso);
+    const alumnos = await CursoModel.listarAlumnos(id_curso);
+    const materiales = await CursoModel.listarMaterial(id_curso);
+    const tareas = await CursoModel.listarTarea(id_curso);
+    const foros = await CursoModel.listarForo(id_curso);
+
+    const data = {
+      profesores,
+      alumnos,
+      materiales,
+      tareas,
+      foros
+    };
+
+    res.render('Cursos/ver', { Curso: data });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error al obtener los datos');
+  }
+});
+
+module.exports = router;
